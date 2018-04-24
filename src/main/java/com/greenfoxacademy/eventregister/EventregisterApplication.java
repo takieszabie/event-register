3e1d7d2ca5563378992c52986ac215a6a6dbfdfc
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-04-24 11:42:46 +0200

    project initiated

---
+++ src/main/java/com/greenfoxacademy/eventregister/EventregisterApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.eventregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventregisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventregisterApplication.class, args);
+	}
+}
